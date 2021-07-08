bbc07f55e8f5238be6c27f6ed32395c27de4abe8
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2021-07-08 17:21:19 +0800

    Initial commit.

---
+++ packages/unique_identifier/src/UniqueIdentifier.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace RPAPP;
+
+use Illuminate\Support\Str;
+
+class UniqueIdentifier
+{
+    const RANDOM_BYTES = 'RANDOM_BYTES';
+    const RANDOM_STRING = 'RANDOM_STRING';
+    const OPENSSL_RANDOM_PSEUDO_BYTES = 'OPENSSL_RANDOM_PSEUDO_BYTES';
+
+    public function generate(string $type, int $length, bool $strong_result = null): string
+    {
+        $result = '';
+
+        $a = trim($type);
+
+        switch($a)
+        {
+            case UniqueIdentifier::RANDOM_BYTES:
+            {
+                $b = random_bytes($length);
+                $result = bin2hex($b);
+
+                break;
+            }
+            case UniqueIdentifier::RANDOM_STRING:
+            {
+                $b = Str::random($length);
+                $result = $b;
+
+                break;
+            }
+            case UniqueIdentifier::OPENSSL_RANDOM_PSEUDO_BYTES:
+            {
+                $b = openssl_random_pseudo_bytes($length, $strong_result);
+                $result = bin2hex($b);
+
+                break;
+            }
+        }
+
+        return $result;
+    }
+}
