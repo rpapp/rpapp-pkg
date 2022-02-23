9a49761d891c7adb96c8d2680b329f33fa75ce43
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-02-23 17:23:59 +0800

    Fix array in `getError()` function.

--- src/LDAPError.php
+++ src/LDAPError.php
@@ -9,58 +9,58 @@ class LDAPError
 	public function __construct() {}
 	public function getError(string $errorCode): array
 	{
-		$message = [];
+		$message = array();
 
 		switch($errorCode) 
 		{
 			case('525'): 
 			{
-				$message = [('code' => '525', 'message' => Messages::USER_NOT_FOUND)];
+				$message = array('code' => '525', 'message' => Messages::USER_NOT_FOUND);
 				break;
 			}
 			case('52e'):
 			{
-				$message = [('code' => '52e', 'message' => Messages::INVALID_CREDENTIALS)];
+				$message = array('code' => '52e', 'message' => Messages::INVALID_CREDENTIALS);
 				break;
 			}
 			case('530'):
 			{
-				$message = [('code' => '530', 'message' => Messages::USER_NOT_PERMITTED_AT_THIS_TIME)];
+				$message = array('code' => '530', 'message' => Messages::USER_NOT_PERMITTED_AT_THIS_TIME);
 				break;
 			}
 			case('531'):
 			{
-				$message = [('code' => '531', 'message' => Messages::USER_NOT_PERMITTED_AT_THIS_WORKSTATION)];
+				$message = array('code' => '531', 'message' => Messages::USER_NOT_PERMITTED_AT_THIS_WORKSTATION);
 				break;
 			}
 			case('532'):
 			{
-				$message = [('code' => '532', 'message' => Messages::PASSWORD_EXPIRED)];
+				$message = array('code' => '532', 'message' => Messages::PASSWORD_EXPIRED);
 				break;
 			}
 			case('533'):
 			{
-				$message = [('code' => '533', 'message' => Messages::ACCOUNT_DISABLED)];
+				$message = array('code' => '533', 'message' => Messages::ACCOUNT_DISABLED);
 				break;
 			}
 			case('701'):
 			{
-				$message = [('code' => '701', 'message' => Messages::ACCOUNT_EXPIRED)];
+				$message = array('code' => '701', 'message' => Messages::ACCOUNT_EXPIRED);
 				break;
 			}
 			case('773'):
 			{
-				$message = [('code' => '773', 'message' => Messages::USER_MIST_RESET_PSSWORD)];
+				$message = array('code' => '773', 'message' => Messages::USER_MIST_RESET_PSSWORD);
 				break;
 			}
 			case('775'):
 			{
-				$message = [('code' => '775', 'message' => Messages::USER_ACCOUNT_LOCKED)];
+				$message = array('code' => '775', 'message' => Messages::USER_ACCOUNT_LOCKED);
 				break;
 			}
 			default:
 			{
-				$message = [('code' => '0', 'message' => Messages::Unknown)];
+				$message = array('code' => '0', 'message' => Messages::Unknown);
 				break;
 			}
 		}
