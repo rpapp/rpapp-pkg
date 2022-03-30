acab6d459a1feb1ebdc650b11965c579f96f5913
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-03-30 23:45:30 +0800

    Added `BAD_REQUEST` const.

--- src/ResponseStatusCodes.php
+++ src/ResponseStatusCodes.php
@@ -7,6 +7,7 @@ class ResponseStatusCodes
     const OK = 200;
     const CREATED = 201;
     const NO_CONTENT = 204;
+    const BAD_REQUEST = 400;
     const NOT_FOUND = 404;
     const NOT_IMPLEMENTED = 501;    
 }
