38c6ac2b362bc528997972a84f5c02001e674c22
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-03-30 23:05:47 +0800

    Added `ResponseStatusCode` class.

---
+++ src/ResponseStatusCodes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RPAPP;
+
+class ResponseStatusCodes
+{
+    const OK = 200;
+    const CREATED = 201;
+    const NO_CONTENT = 204;
+    const NOT_FOUND = 404;
+    const NOT_IMPLEMENTED = 501;    
+}
