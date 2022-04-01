e5bfcbb78968eb7f6951e657686c30a7f7aa5642
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-04-01 15:23:24 +0800

    Added `INTERNAL_SERVER_ERROR` const.

--- src/ResponseStatusCodes.php
+++ src/ResponseStatusCodes.php
@@ -9,5 +9,6 @@ class ResponseStatusCodes
     const NO_CONTENT = 204;
     const BAD_REQUEST = 400;
     const NOT_FOUND = 404;
+    const INTERNAL_SERVER_ERROR = 500;
     const NOT_IMPLEMENTED = 501;    
 }
