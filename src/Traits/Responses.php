14e228e55b336ae70a3bcc0ac7ea61975cce0a4c
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-04-15 14:00:29 +0800

    Added `exception` variable.

--- src/Traits/Responses.php
+++ src/Traits/Responses.php
@@ -5,6 +5,7 @@ namespace RPAPP\Traits;
 trait Responses
 {
     public ?string $code = null;
+    public ?string $exception = null;
     public ?string $message = null;
     public ?string $logMessage = null;
     public ?string $timestamp = null;
