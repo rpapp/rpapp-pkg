1f70957bb7b04d96abee80ce84e897da25122ec3
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-04-16 01:00:32 +0800

    Added `State` class with const.

---
+++ src/State.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RPAPP;
+
+class State
+{
+    const FALSE = 0;
+    const TRUE = 1;
+    const ENABLED = 2;
+    const DISABLED = 3;
+    const ACTIVE = 4;
+    const INACTIVE = 5;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {}
+}
