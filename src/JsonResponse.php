55f62ac4eef0d1a44582e86f0b1e3e7ef0f3bf97
Raphael D. Pfefferle <RDPfefferle@swhealth.com>
2022-05-27 21:03:25 +0800

    Removed duplicate `setMessage()` function. Clean the code.

--- src/JsonResponse.php
+++ src/JsonResponse.php
@@ -12,9 +12,8 @@ class JsonResponse
 {
     /**
      * Get invalid API or resouce not found message.
-     * 
      * @return object|null
-     */
+     **/
     public function getInvalidAPIOrResourceNotFound(): ?object
     {
         return response()->json([
@@ -24,168 +23,13 @@ class JsonResponse
             'timestamp' => Carbon::now()->toDateTimeString()
         ], ResponseStatusCode::NOT_IMPLEMENTED);
     }
-
-    public function setMessage(int $responseStatusCode, string $message, string $logMessage): ?object
-    {
-        return response()->json([
-            'code' => $responseStatusCode,
-            'message' => trim($message),
-            'logMessage' => trim($logMessage),
-            'timestamp' => Carbon::now()->toDateTimeString()
-        ]);
-    }
-
-    // public function getUserFound(object $object): ?object
-    // {
-    //     $responseObject = (object) [];
-    //     $responseObject = $object;
-    //     $responseObject['code'] = ResponseStatusCodes::OK;
-    //     $responseObject['message'] = Messages::USER_FOUND;
-    //     $responseObject['logMessage'] = LogMessages::USER_FOUND;
-    //     $responseObject['timestamp'] = Carbon::now()->toDateTimeString();
-
-    //     return response()->json($responseObject, ResponseStatusCodes::OK);
-    // }
-
-    // public function getUserNotFound(): ?object
-    // {
-    //     $responseObject = (object) [];                    
-    //     $responseObject->code = ResponseStatusCodes::NOT_FOUND;
-    //     $responseObject->message = Messages::USER_NOT_FOUND;
-    //     $responseObject->logMessage = LogMessages::USER_NOT_FOUND;
-    //     $responseObject->timestamp = Carbon::now()->toDateTimeString();
-
-    //     return response()->json($responseObject, ResponseStatusCodes::NOT_FOUND);
-    // }
     /**
-     * Get query exception message.
-     * 
-     * @param string|null $code
-     * @param string|null $message
-     * @param string|null $file
-     * @param string|null $line
-     * @param string|null $trace
-     * 
-     * @return object|null
-     */
-    // public function getQueryException(string $code = null, string $message = null, string $file = null, string $line = null, string $trace = null): ?object
-    // {
-    //     $responseObject = (object) [];                    
-        
-    //     if($code == 0 || $code == null || $code == '')
-    //     {
-    //         $responseObject->code = ResponseStatusCodes::BAD_REQUEST;
-    //     }
-    //     else
-    //     {
-    //         $responseObject->code = $code;
-    //     }
-        
-    //     $responseObject->message = $message;
-    //     $responseObject->logMessage = LogMessages::QUERY_EXCEPTION;
-    //     $responseObject->timestamp = Carbon::now()->toDateTimeString();
-    //     $responseObject->file = $file;
-    //     $responseObject->line = $line;
-    //     $responseObject->trace = $trace;
-
-    //     return response()->json($responseObject, ResponseStatusCodes::BAD_REQUEST);
-    // }
-    /**
-     * Get exception message.
-     * 
-     * @param string|null $code
-     * @param string|null $message
-     * @param string|null $logMessage
-     * @param string|null $file
-     * @param string|null $line
-     * @param string|null $trace
-     * 
-     * @return object|null
-     */
-    // public function getException(string $code = null, string $codeMessage = null, string $message = null, string $logMessage = null, string $file = null, string $line = null, string $trace = null): ?object
-    // {
-    //     // $responseObject = (object) [];
-
-    //     // if($code == 0 || $code == null || $code == '')
-    //     // {
-    //     //     $responseObject->code = ResponseStatusCodes::INTERNAL_SERVER_ERROR;
-    //     // }
-    //     // else
-    //     // {
-    //     //     $responseObject->code = $code;
-    //     // }
-
-    //     // $responseObject->message = $message;
-    //     // $responseObject->logMessage = $logMessage;
-    //     // $responseObject->timestamp = Carbon::now()->toDateTimeString();
-    //     // $responseObject->file = $file;
-    //     // $responseObject->line = $line;
-    //     // $responseObject->trace = $trace;
-
-    //     // return response()->json($responseObject, ResponseStatusCodes::INTERNAL_SERVER_ERROR);
-    //     return response()->json([
-    //         'code' => $code,
-    //         'codeMessage' => $codeMessage,
-    //         'message' => $message,
-    //         'logMessage' => $logMessage,
-    //         'timestamps' => Carbon::now()->toDateTimeString(),
-    //         'exceptionDetails' =>
-    //         [
-    //             'file' => $file,
-    //             'line' => $line,
-    //             'trace' => $trace
-    //         ]
-    //     ]);
-    // }
-
-    // public function getCreated(string $message, string $logMessage): ?object
-    // {
-    //     $responseObject = (object) [];                    
-    //     $responseObject->code = ResponseStatusCodes::OK;
-    //     $responseObject->message = $message;
-    //     $responseObject->logMessage = $logMessage;
-    //     $responseObject->timestamp = Carbon::now()->toDateTimeString();       
-
-    //     return response()->json($responseObject, ResponseStatusCodes::OK);
-    // }
-
-    // public function getUpdated(string $message, string $logMessag): ?object
-    // {
-    //     $responseObject = (object) [];                    
-    //     $responseObject->code = ResponseStatusCodes::OK;
-    //     $responseObject->message = $message;
-    //     $responseObject->logMessage = $logMessage;
-    //     $responseObject->timestamp = Carbon::now()->toDateTimeString();       
-
-    //     return response()->json($responseObject, ResponseStatusCodes::OK);
-    // }   
-    /**
-     * Generic or common response message.
-     * 
-     * @param string|null $responseStatusCodes 
-     * @param string|null $message
-     * @param string|null $logMessage
-     * 
-     * @return object|null
-     */
-    // public function getMessage(string $responseStatusCodes = null, string $message = null, string $logMessage = null): ?object
-    // {
-    //     return response()->json([
-    //         'code' => $responseStatusCodes,
-    //         'message' => $message,
-    //         'logMessage' => $logMessage,
-    //         'timestamp' => Carbon::now()->toDateTimeString()
-    //     ], $responseStatusCodes);
-    // }
-    /**
-     * Set generic or common response message.
-     * 
+     * Set message.
      * @param string|null $responseStatusCode
      * @param string|null $message
      * @param string|null $logMessage
-     * 
      * @return object|null
-     */
+     **/
     public function setMessage(int $responseStatusCode = null, string $message = null, string $logMessage = null): ?object
     {
         return response()->json([
@@ -197,9 +41,8 @@ class JsonResponse
     }
     /**
      * Get internal server error message.
-     * 
      * @return object|null
-     */
+     **/
     public function getInternalServerError(): ?object
     {
         return response()->json([
@@ -209,22 +52,4 @@ class JsonResponse
             'timestamp' => Carbon::now()->toDateTimeString()
         ], ResponseStatusCode::INTERNAL_SERVER_ERROR);
     }
-    /**
-     * Get not found message.
-     * 
-     * @param string|null $message
-     * @param string|null $logMessage
-     * 
-     * @return object|null
-     */
-    // public function getNotFound(string $message = null, string $logMessage = null): ?object
-    // {
-    //     $responseObject = (object) [];                    
-    //     $responseObject->code = ResponseStatusCodes::NOT_FOUND;
-    //     $responseObject->message = $message;
-    //     $responseObject->logMessage = $logMessage;
-    //     $responseObject->timestamp = Carbon::now()->toDateTimeString();
-
-    //     return response()->json($responseObject, ResponseStatusCodes::NOT_FOUND);
-    // }
 }
